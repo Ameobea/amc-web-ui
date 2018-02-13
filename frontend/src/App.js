dd8c626091ec2c6767d35bd2189650662a9be0eb
Jackson Roush <jacksonroush@gmail.com>
2018-02-13 15:56:15 -0600

    Added answerField class, CSS, updated form to reflect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import {
   HelpBlock,
 } from "react-bootstrap";
 import "./App.css";
+import {AnswerField} from "./answerField.js";
 
 const FieldGroup = ({ id, label, help, ...props }) => (
   <FormGroup controlId={id}>
@@ -18,11 +19,11 @@ const FieldGroup = ({ id, label, help, ...props }) => (
 
 class App extends React.Component {
   handleSubmit = () => {
-    alert(this.questionNameRef.value);
+    alert(this.questionNameRef.value + this.answer1Ref.value + this.answer2Ref.value + this.answer3Ref.value + this.answer4Ref.value);
   }
 
   render = () => (
-    <div style={{ width: 500, marginLeft: 20 }}>
+    <div style={{ width: 520, marginLeft: 20 }}>
       <form>
         <FieldGroup
           type="text"
@@ -30,6 +31,37 @@ class App extends React.Component {
           placeholder="Enter text"
           inputRef={ref => this.questionNameRef = ref}
         />
+        <div style={{ width: 500, marginLeft: 20 }}>
+          <AnswerField
+            type="text"
+            label="Answer 1"
+            placeholder="Enter text"
+          />
+          <AnswerField
+            type="text"
+            label="Answer 2"
+            placeholder="Enter text"
+            inputRef={ref => this.answer2Ref = ref}
+          />
+          <AnswerField
+            type="text"
+            label="Answer 3"
+            placeholder="Enter text"
+            inputRef={ref => this.answer3Ref = ref}
+          />
+          <AnswerField
+            type="text"
+            label="Answer 4"
+            placeholder="Enter text"
+            inputRef={ref => this.answer4Ref = ref}
+          />
+          <AnswerField
+            type="text"
+            label="Answer 5"
+            placeholder="Enter text"
+            inputRef={ref => this.answer5Ref = ref}
+          />
+        </div>
         <Button
           bsStyle="primary"
           onClick={this.handleSubmit}
---
+++ frontend/src/answerField.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import {
+  Button,
+  FormControl,
+  ControlLabel,
+  FormGroup,
+  HelpBlock,
+} from "react-bootstrap";
+import "./App.css";
+import "./answerField.css";
+
+const FieldGroup = ({ id, label, help, ...props }) => (
+  <FormGroup controlId={id}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl {...props} />
+    {help && <HelpBlock>{help}</HelpBlock>}
+  </FormGroup>
+);
+
+export class AnswerField extends React.Component
+{
+  render()
+  {
+    return(
+    <div className="answerContainer">
+      <input id="checkBox" type="checkbox" style={{width: 20}}/>
+      <FieldGroup
+        type={this.props.text}
+        label={this.props.label}
+        placeholder={this.props.placeholder}
+        ref = {this.inputRef}
+        style = {{width: 490}}
+      />
+    </div>)
+  }
+}
