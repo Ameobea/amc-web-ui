d0f41ab7c900fc5edf8c07d40cb4230695bca242
Jackson Roush <jacksonroush@gmail.com>
2018-02-15 16:13:12 -0600

    Added QuestionField component and a question field to form

--- frontend/src/form.js
+++ frontend/src/form.js
@@ -10,6 +10,7 @@ import { compose, withState } from 'recompose';
 
 import "./App.css";
 import { AnswerField } from "./answerField.js";
+import { QuestionField } from "./question.js";
 import "./answerField.css";
 
 const PopulatedAnswerField = ({ values, setValues, name, ...props }) => (
@@ -19,6 +20,15 @@ const PopulatedAnswerField = ({ values, setValues, name, ...props }) => (
     name={name}
     {...props}
   />
+)
+
+const PopulatedQuestionField = ({ values, setValues, name, ...props }) => (
+    <QuestionField
+      onChange={e => setValues({ ...values, [name]: e.target.value })}
+      value={values[name]}
+      name={name}
+      {...props}
+    />
 )
 
 const Answers = ({ answerNames, setanswerNames, values, setValues }) => (
@@ -39,7 +49,7 @@ const Answers = ({ answerNames, setanswerNames, values, setValues }) => (
         label: `Answer ${answerNames.length + 1}`,
       }
     ])}>
-      Add Question
+      Add Answer
     </Button>
   </div>
 )
@@ -51,6 +61,11 @@ const handleSubmit = values => {
 const Form = ({ values, setValues, answerNames, setanswerNames }) => (
   <div style={{ width: 520, marginLeft: 20 }}>
     <form>
+    <PopulatedQuestionField
+      label = "Question"
+      name = 'question'
+      values={values}
+      setValues={setValues}/>
       <div style={{ width: 500, marginLeft: 20 }}>
         <Answers
           answerNames={answerNames}
@@ -76,5 +91,5 @@ export default compose(
     {label: 'Answer 1', name: 'answer1'},
     {label: 'Answer 2', name: 'answer2'},
     {label: 'Answer 3', name: 'answer3'},
-  ]),
+  ])
 )(Form);
