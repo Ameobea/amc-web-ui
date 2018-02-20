96e8b679ee4d0beddcba3bf6367c1fde7312fb69
Casey Primozic <me@ameo.link>
2018-02-20 16:33:37 -0600

    Initial Backend<->Frontend Integration
     * Remove serviceworker from frontend because it was causing incredibly annoying caching issues
     * Integrate the TeX generation script into the backend webserver
     * Add CORS support to the backend webserver
     * Create initial Dockerfile for deploying the application
       * Build the frontend
       * Installs python dependencies and start the webserver

--- backend/ToTEX.py
+++ backend/ToTEX.py
@@ -1,14 +1,18 @@
 import json
-q = json.loads("{\"questionName\":\"Test Question\",\"questionText\":\"How many planets are there in the solar system?\",\"answers\":[{\"answerText\":\"8\",\"correct\":true},{\"answerText\":\"3\",\"correct\":false}],\"meta\":{\"anySelectedAreCorrect\":false},\"points\":1}")
-t = open('question.tex', 'w')
-t.write('\\begin{question}{01-a}\n')
-t.write('\t' + q['questionText'] + '\n')
-t.write('\t\\begin{choices}\n')
-for a in q['answers']:
-	if a['correct']:
-		t.write('\t\t\\correctchoice{' + a['answerText'] + '}\n')
-	else:
-		
-		t.write('\t\t\\wrongchoice{' + a['answerText'] + '}\n')
-t.write('\t\\end{choices}\n')
-t.write('\\end{question}\n')
+
+def parse_dict(q: dict) -> str:
+    output = ""
+
+    output += '\\begin{question}{01-a}\n'
+    output += '\t' + q['questionText'] + '\n'
+    output += '\t\\begin{choices}\n'
+    for a in q['answers']:
+        if a['correct']:
+            output += '\t\t\\correctchoice{' + a['answerText'] + '}\n'
+        else:
+
+            output += '\t\t\\wrongchoice{' + a['answerText'] + '}\n'
+    output += '\t\\end{choices}\n'
+    output += '\\end{question}\n'
+
+    return output
