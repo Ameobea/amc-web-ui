67217e2e2127a7f89b5f3b31b21bce04cc306553
Casey Primozic <me@ameo.link>
2018-03-28 16:28:29 -0500

    Update AMC wrapper methods
     * Create new project directory from scratch rather than copying in
     * Handle paths a bit better

--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -1,8 +1,11 @@
 import json
 import pythonWrapper
+from os import path
 
 from flask import Flask, request
 from flask_cors import CORS
+
+
 
 from ToTEX import parse_dict
 
@@ -14,28 +17,27 @@ def process_question():
     print(request.json)
 
     body = request.json
-#    body['a'] = body['a'] + 1
-
-
-	
     return json.dumps(body)
 
 @app.route("/generate_tex", methods=["POST"])
 def generate_tex():
     j = request.json
-    
+
     return parse_dict(j)
 
 @app.route("/create_project", methods=["POST"])
 def generate_pdf():
     j = request.json
-    quiz = open('test.tex', 'w')
-    quiz.write(parse_dict(j))
-    quiz.close()
-    pythonWrapper.createProject('pythonTest4', pythonWrapper.getDirFromConfig()+ '/')
-    pythonWrapper.addQuestion(pythonWrapper.getSampleTexFileLocation() + 'simple.tex', pythonWrapper.getDirFromConfig(), 'pythonTest4')
-    pythonWrapper.prepareQuestion(pythonWrapper.getDirFromConfig()+'/'+'pythonTest4', 'simple.tex', 'TheNameOfThePDF')
-#    return "Success"
-    return pythonWrapper.getDirFromConfig()
+
+    with open('text.tex', mode='w') as quiz_file:
+        quiz_file.write(parse_dict(j))
+        quiz_file.close()
+
+    project_name = 'pythonTest4'
+    project_dir = pythonWrapper.createProject(project_name)
+    pythonWrapper.addQuestion('text.tex', project_dir, project_name)
+    pythonWrapper.prepareQuestion(project_dir, 'text.tex', 'TheNameOfThePDF')
+
+    return project_dir
 
 app.run(host='0.0.0.0', port=4545)
--- backend/pythonWrapper.py
+++ backend/pythonWrapper.py
@@ -1,32 +1,43 @@
+from functools import partial
+import tempfile
 import os
+from os import path
 import json
+from typing import List
 
-def createProject(projectName, projectLocation):	
-  os.system('sh createAMCProjectTest.sh ' + projectName+ ' ' + projectLocation + ' ' + getEmptyProjectLcation())
-  return
+def make_project_dir(temp_dir: str, paths: List[str]):
+  os.mkdir(path.join(temp_dir, *paths))
+
+def createProject(project_name: str):
+  """ Creates a new project in a temporary directory and returns the path to the
+  created directory. """
+
+  temp_dir = tempfile.mkdtemp()
+
+  # Set up the directory with the AMC project structure
+  create_dir = partial(make_project_dir, temp_dir)
+  create_dir([project_name])
+
+  def create_inner_dir(dirs): return make_project_dir(
+      temp_dir, [project_name, *dirs])
+  create_inner_dir(['cr'])
+  create_inner_dir(['cr', 'corrections'])
+  create_inner_dir(['cr', 'corrections', 'jpg'])
+  create_inner_dir(['cr', 'corrections', 'pdf'])
+  create_inner_dir(['cr', 'diagnostic'])
+  create_inner_dir(['cr', 'zooms'])
+  create_inner_dir(['data'])
+  create_inner_dir(['exports'])
+  create_inner_dir(['scans'])
+  create_inner_dir(['copies'])
+
+  return temp_dir
 
 def addQuestion(texFile, projectLocation, projectName):
   os.system('sh addAQuestion.sh ' + texFile + ' ' + projectLocation + ' ' + projectName)
-  return
 
 def prepareQuestion(projectDir, questionSourceFile, pdfName):
-  os.system('sh prepareQuestion.sh ' + projectDir + ' ' + questionSourceFile +' '+pdfName)
-  return
-
-def getDirFromConfig():
-  config = open("config.json","r")
-#  print(config.read())
-  config_json = json.loads(config.read())
-  config.close()
-#  print(config_json['project_location'])
-  return config_json['project_location']
-
-def getSampleTexFileLocation():
-  config = open("config.json","r")
-  config_json = json.loads(config.read())
-  config.close()
-  return config_json['sample_tex_files']
-  
+  os.system('sh prepareQuestion.sh ' + projectDir + ' ' + questionSourceFile + ' ' + pdfName)
 
 def getEmptyProjectLcation():
   config = open("config.json","r")
@@ -35,10 +46,11 @@ def getEmptyProjectLcation():
   return config_json['empty_project_location']
 
 if __name__ == "__main__":
+  project_name = 'pythonTest4'
+  project_dir = createProject(project_name)
+  addQuestion('simple.tex', project_dir, project_name)
+  prepareQuestion(project_dir, project_name, 'simple.tex', 'TheNameOfThePDF')
 
-  createProject('pythonTest4', getDirFromConfig()+ '/')
-  addQuestion(getSampleTexFileLocation() + 'simple.tex', getDirFromConfig(), 'pythonTest4')
-  prepareQuestion(getDirFromConfig()+'/'+'pythonTest4', 'simple.tex', 'TheNameOfThePDF')
 '''
   createProject('pythonTest4', '/home/bill/Documents/AMCScripts')
   addQuestion('/home/bill/MC-Projects/sampleQuestions/simple.tex', '/home/bill/Documents/AMCScripts/', 'pythonTest4')
