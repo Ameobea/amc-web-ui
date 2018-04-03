aca5e9bbe03b549cb75a74c1003dd36dcf3f7ae2
Casey Primozic <me@ameo.link>
2018-04-03 15:53:22 -0500

    Add the ability to have multiple questions
     * Add a button to add additional questions
     * Change state to be an array of questions rather than a single question
     * Update the question form to display all questions in a list, mapped from the state
     * Update validation to check that all questions are valid before allowing submission

--- frontend/src/components/QuestionForm.js
+++ frontend/src/components/QuestionForm.js
@@ -9,6 +9,17 @@ import { QuestionField } from "./Question";
 import "./AnswerField.css";
 import "./QuestionForm.css";
 
+const getEmptyQuestion = () => ({
+  questionText: '',
+  answers: [
+    {answerText: '', correct: false},
+    {answerText: '', correct: false},
+    {answerText: '', correct: false}
+  ],
+  // Does question contain text?
+  questionValid: false
+});
+
 const Answers = ({ answers, setAnswers }) => (
   <div className="answers" style={{ marginBottom: 20 }}>
     {answers.map(({ answerText, correct }, index) => (
@@ -42,71 +53,94 @@ const Answers = ({ answers, setAnswers }) => (
   </div>
 );
 
-//Tests if given input has text
+// Tests if given input has text
 const answerHasText = (answer) => {
   return answer.answerText !== '';
-}
+};
 
 const handleSubmit = state => {
-  //An array of boolean values stating whether or not each answer has text
-  const answersValid = state.answers.map(answerHasText);
-  //Checks for question test and false in answersValid array
-  if (state.questionText !== '' && !answersValid.includes(false)){
-  fetch(`./create_project`, {
+  // An array of boolean values stating whether or not each answer has text
+  const answersValid = state.reduce(
+    (acc, { questionValid, answers }) => acc && questionValid && !answers.find(answer => !answerHasText(answer)),
+    true
+  );
 
-    method: 'POST',
-    body: JSON.stringify(state),
-    headers: {
-      'Content-Type': 'application/json'
-    }}
+  if(!answersValid) {
+    return alert("All fields are required");
+  }
+
+  fetch(
+    './create_project',
+    {
+      method: 'POST',
+      body: JSON.stringify(state),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
   )
     .then(res => res.blob())
     .then(blob => download(blob, 'quiz.pdf', 'application/pdf'));
-  }
-  else
-  {
-    alert("All fields are required");
-  }
 };
+
+const Question = ({ state, setState }) => (
+  <div style={{ paddingBottom: 25 }}>
+    <QuestionField
+      label="Question: "
+      name='question'
+      id='question'
+      value={state.questionText}
+      // Changes question state and questionValid state
+      onChange={e => setState({ ...state, questionText: e.target.value, questionValid: true })}
+    />
+
+    <div style={{ width: 500, marginLeft: 20, paddingTop: 25 }}>
+      <Answers
+        answers={state.answers}
+        setAnswers={newAnswers => setState({ ...state, answers: newAnswers })}
+      />
+    </div>
+  </div>
+);
 
 const Form = ({ state, setState }) => (
   <div className="form" style={{ width: 520, marginLeft: 20 }}>
     <form>
-      <QuestionField
-        label="Question: "
-        name='question'
-        id = 'question'
-        value={state.questionText}
-        //Changes questionText state
-        onChange={e => setState({ ...state, questionText: e.target.value})}
-      />
-
-      <div style={{ width: 500, marginLeft: 20, paddingTop: 25 }}>
-        <Answers
-          answers={state.answers}
-          setAnswers={newAnswers => setState({ ...state, answers: newAnswers })}
-        />
+      <div>
+        {state.map((questionState, i) => (
+          <Question
+            key={i}
+            state={questionState}
+            setState={newState => setState(R.update(i, newState, state))}
+          />
+        ))}
       </div>
 
-      <Button
-        id = "submitButton"
-        bsStyle="primary"
-        onClick={() => handleSubmit(state)}
-      >
-        Submit
-      </Button>
+      <hr />
+
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Button
+          id="submitButton"
+          bsStyle="primary"
+          onClick={() => setState([...state, getEmptyQuestion()])}
+          style={{ marginTop: 0, marginRight: 10 }}
+        >
+          Add Question
+        </Button>
+
+        <Button
+          id="submitButton"
+          bsStyle="primary"
+          onClick={() => handleSubmit(state)}
+        >
+          Submit
+        </Button>
+      </div>
     </form>
   </div>
 );
 
-const initialState = {
-  questionText: '',
-  answers: [
-    {answerText: '', correct: false},
-    {answerText: '', correct: false},
-    {answerText: '', correct: false}
-  ]
-};
+const initialState = [getEmptyQuestion()];
 
 export default compose(
   withState('state', 'setState', initialState),
