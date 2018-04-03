a2b2663c6be0f3928dc5268299ba692fcdbfb61d
Benjamin Levandowski <benjamin.levandowski@valpo.edu>
2018-04-03 15:56:13 -0500

    Parses lists of dicts

--- backend/ToTEX.py
+++ backend/ToTEX.py
@@ -61,9 +61,15 @@ def parse_dict(q: dict) -> str:
         if a['correct']:
             output += '\t\t\\correctchoice{' + a['answerText'] + '}\n'
         else:
-
             output += '\t\t\\wrongchoice{' + a['answerText'] + '}\n'
     output += '\t\\end{choices}\n'
     output += '\\end{question}\n'
 
+    return output
+
+def parse_dict_list(l: list) -> str:
+    output = ""
+    for q in list:
+        output += parse_dict(q)
+        output += "\n"
     return header + output + trailer
